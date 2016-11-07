fccbecc251b3a3b94c864611370f719f6db2a0be
Benedikt Geißler <benedikt@g5r.eu>
2016-11-07 12:23:51 +0100

    Complete tut2 task 1

--- tut2/task1.hs
+++ tut2/task1.hs
@@ -4,6 +4,16 @@ calcFibonacci n =
        then 1
        else calcFibonacci (n - 1) + calcFibonacci (n - 2)
 
-calcPi :: Int -> Float
-calcPi n =
-    -- do stuff
+calcPi2 :: Int -> Float
+calcPi2 n
+    | n == 0 = 1
+    | otherwise = (-1)^n / fromIntegral (2 * n + 1) + calcPi2 (n-1)
+
+calcPi :: Float
+calcPi = 4 * calcPi2 10000
+
+ggT :: Int -> Int -> Int
+ggT a b
+    | a > b = ggT (a - b) b
+    | a < b = ggT (b - a) a
+    | otherwise = a
