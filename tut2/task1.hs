53fc3e32b952f7743cb72515dfb12ba70bed69f6
Benedikt Geißler <benedikt@g5r.eu>
2016-11-07 02:52:24 +0100

    Prepare tut2 task 1b

--- tut2/task1.hs
+++ tut2/task1.hs
@@ -1,4 +1,9 @@
-calcFibonacci :: Integer -> Integer
+calcFibonacci :: Int -> Int
 calcFibonacci n =
-    if (n <= 2) then 1
+    if n <= 2
+       then 1
        else calcFibonacci (n - 1) + calcFibonacci (n - 2)
+
+calcPi :: Int -> Float
+calcPi n =
+    -- do stuff
