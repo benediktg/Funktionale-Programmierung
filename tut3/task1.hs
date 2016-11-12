1e12aee4fe8b578a980624bb8dbb6d09e5be0082
Benedikt Geißler <benedikt@g5r.eu>
2016-11-12 22:36:44 +0100

    Add tut3 task 1

---
+++ tut3/task1.hs
@@ -0,0 +1,16 @@
+orderTriple :: (Int, Int, Int) -> (Int, Int, Int)
+orderTriple (a, b, c)
+    | a > b = orderTriple (b, a, c)
+    | a > c = orderTriple (c, b, a)
+    | b > c = orderTriple (a, c, b)
+    | otherwise = (a, b, c)
+
+rotateQuadruple :: (Int, Int, Int, Int) -> Int -> (Int, Int, Int, Int)
+rotateQuadruple (a, b, c, d) n
+    | n > 0 = rotateQuadruple (d, a, b, c) (n - 1)
+    | n < 0 = rotateQuadruple (b, c, d, a) (n + 1)
+    | otherwise = (a, b, c, d)
+
+calcAddSubMulDiv :: Float -> Float -> (Float, Float, Float, Float)
+calcAddSubMulDiv a b =
+    (a + b, a - b, a * b, a / b)
