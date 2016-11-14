db164cfd75a47618ee647582f38930e0593440f3
Benedikt Geißler <benedikt@g5r.eu>
2016-11-14 13:01:05 +0100

    Improve functions

--- tut3/task1.hs
+++ tut3/task1.hs
@@ -6,8 +6,8 @@ orderTriple (a, b, c)
 
 rotateQuadruple :: (Int, Int, Int, Int) -> Int -> (Int, Int, Int, Int)
 rotateQuadruple (a, b, c, d) n
-    | n > 0 = rotateQuadruple (d, a, b, c) (n - 1)
-    | n < 0 = rotateQuadruple (b, c, d, a) (n + 1)
+    | n > 0 = rotateQuadruple (d, a, b, c) (mod (n - 1) 4)
+    | n < 0 = rotateQuadruple (b, c, d, a) (mod (n + 1) 4)
     | otherwise = (a, b, c, d)
 
 calcAddSubMulDiv :: Float -> Float -> (Float, Float, Float, Float)
--- tut3/task2.hs
+++ tut3/task2.hs
@@ -1,24 +1,18 @@
 numOfOccur :: [Int] -> Int -> Int
-numOfOccur xs x = numOfOccurRec xs x 0
-
-numOfOccurRec :: [Int] -> Int -> Int -> Int
-numOfOccurRec [] x n = n
-numOfOccurRec xs x n
-    | (head xs) == x = numOfOccurRec (tail xs) x (n + 1)
-    | otherwise = numOfOccurRec (tail xs) x n
+numOfOccur [] _ = 0
+numOfOccur (x:xs) a = (if (a == x) then 1 else 0) + numOfOccur xs a
 
 isPalin :: [Int] -> Bool
-isPalin xs = isPalinRec xs (reverse xs)
-
-isPalinRec :: [Int] -> [Int] -> Bool
-isPalinRec [] [] = True
-isPalinRec xs xsRev
-    | head xs == head xsRev = isPalinRec (tail xs) (tail xsRev)
-    | otherwise = False
+isPalin xs = xs == reverse xs
 
 primeTwins :: Int -> [(Int, Int)]
-primeTwins x =
-    [(a - 2, a) | a <- [(1 + 2)..x], prime a && prime (a - 2)]
+primeTwins x = [(a - 2, a) | a <- [3..x], prime a && prime (a - 2)]
+
+-- primeTwins' :: Int -> [(Int, Int)] -> [(Int, Int)]
+-- primeTwins' x l
+--     | x < 5 = l
+--     | prime x && prime (x - 2) = primeTwins' (x - 2) ((x - 2, x):l)
+--     | otherwise primeTwins' (x - 1) l
 
 prime :: Int -> Bool
 prime n = (divisors n == [1,n])
@@ -27,7 +21,12 @@ divisors :: Int -> [Int]
 divisors n = [d | d <- [1..n], n `mod` d == 0]
 
 splitList :: [(Int, Int)] -> ([Int], [Int])
-splitList xs =
-    let ys = [fst x | x <- xs]
-        zs = [snd x | x <- xs]
-    in (ys, zs)
+splitList xs = ([a | (a, _) <- xs], [b | (_, b) <- xs])
+
+-- splitList' :: [(Int, Int)] -> ([Int], [Int])
+-- splitList' xs = (splitListL xs, splitListR xs)
+--
+-- splitListL :: [(Int, Int)] -> [Int] -> [Int]
+-- splitListL ((a, _):xs) =
+--
+-- splitListR :: [(Int, Int)] -> [Int] -> [Int]
