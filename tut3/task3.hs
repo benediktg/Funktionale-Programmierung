3e46cf608cc07327278cdc81034ee8cf8290024d
Benedikt Geißler <benedikt@g5r.eu>
2016-11-14 02:08:40 +0100

    Fix mergeSort

--- tut3/task3.hs
+++ tut3/task3.hs
@@ -4,17 +4,16 @@ sumNorm xs
     | otherwise = abs (head xs) + sumNorm (tail xs)
 
 mergeSort :: [Int] -> [Int]
-mergeSort xs
-    | length xs <= 1 = xs
-    | otherwise = let xs1 = take (div (length xs) 2) xs
-                      xs2 = drop (div (length xs) 2) xs
-                  in reverse (merge (mergeSort xs1) (mergeSort xs2))
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs =
+    let xs1 = take (div (length xs) 2) xs
+        xs2 = drop (div (length xs) 2) xs
+    in merge (mergeSort xs1) (mergeSort xs2)
 
 merge :: [Int] -> [Int] -> [Int]
+merge xs [] = xs
+merge [] ys = ys
 merge (x:xs) (y:ys)
-    | not (null xs) && not (null ys) = if x <= y
-                                          then (x:(merge xs (y:ys)))
-                                          else (y:(merge (x:xs) ys))
-    | null ys = (x:(merge xs []))
-    | null xs = (y:(merge [] ys))
-    | otherwise = []
+    | x <= y = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
