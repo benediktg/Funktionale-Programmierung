49e5a809791ed1513131166d47499b76e23cd199
Benedikt Geißler <benedikt@g5r.eu>
2016-11-28 12:59:48 +0100

    Add tut5 task 1a)

---
+++ tut5/task1.hs
@@ -0,0 +1,6 @@
+data FloatExpr = Lit Float
+               | Add FloatExpr FloatExpr
+               | Mult FloatExpr FloatExpr
+               | Sqrt FloatExpr FloatExpr
+               | Abs FloatExpr
+
