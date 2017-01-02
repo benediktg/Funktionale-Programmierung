81ce49a5efd3c98a7b7e35303124cf4d3ec29fcd
Benedikt Geissler <benedikt@g5r.eu>
2017-01-02 14:12:04 +0100

    Add tasks from tutorial 7

---
+++ tut7/task2.hs
@@ -0,0 +1,19 @@
+-- a)
+getInt :: IO Int
+getInt = getLine
+    >>= return.read
+
+-- b)
+getNInts :: Int -> IO [Int]
+getNInts n
+    | n <= 0 = return []
+    | otherwise = do i <- getInt
+                     is <- getNInts (n - 1)
+                     return (i:is)
+
+-- c)
+main = do putStrLn "Bitte Anzahl eingeben!"
+          n <- getInt
+          putStrLn ("Bitte " ++ (show n) ++ " Integer eingeben")
+          xs <- getNInts n
+          putStrLn ("Die Summe von " ++ show xs ++ " ist " ++ show (sum xs))
